9e2f8cc7420bb7e816d82fa0270142db156ba1ed
daniil <daniilzhilyaev@gmail.com>
2023-07-25 21:08:49 +0300

    add Example 0010 Method Array

---
+++ Example0010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 2, 4, 6, 2, 45, 34, 2, 6 };
+int index = 0;
+int n = array.Length;
+int find = 6;
+
+while(index < n)
+{
+    if(array[index] == find) 
+    {
+        Console.WriteLine(index);
+        break;
+        };
+    index++;
+}
